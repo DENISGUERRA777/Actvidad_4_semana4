ca0fe7fb76c5a85d15fa29a0e5d4f50a8fade051
DENISGUERRA777 <denisgxm@gmail.com>
2024-11-23 16:35:53 -0600

    first commit

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+/*Ejercicio 1
+Problema de Ordenar Lista con Bubble Sort:
+Escribe un programa que ordene una lista de números de forma descendente
+utilizando el algoritmo Bubble Sort. La lista puede contener duplicados
+y valores negativos. Asegúrate de manejar estos casos y muestra la lista 
+antes y después de aplicar el algoritmo.*/
+
+
+/*Ejercicio 2
+Problema de Ordenar Lista con Merge Sort:
+Implementa una función que ordene una lista de palabras alfabéticamente
+utilizando el algoritmo Merge Sort. Muestra la lista antes y después de
+aplicar el algoritmo.*/
+
+
+/*Ejercicio 3
+Problema de Ordenar Lista con Insertion Sort:
+Crea un script que ordene una lista de nombres en orden alfabético
+utilizando el algoritmo Insertion Sort. Muestra la lista antes y 
+después de aplicar el algoritmo.*/
+
+?>
